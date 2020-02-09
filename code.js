c1d3c5192240df54946106065938a03f9288b711
MinuhaFire <winbeta040@gmail.com>
2020-02-09 20:32:21 +0700

    Update code.js

--- code.js
+++ code.js
@@ -602,9 +602,5 @@ bot.on('message', async message => {
 
 
 bot.login(bot_token)
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
+
           
